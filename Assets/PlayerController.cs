555efdd9b39166e07c42b955d886967252ac071c
Adhiraj <adhirajsingh0105@gmail.com>
2020-05-13 00:41:12 +0530

    initial setup

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    private PlayerMotor motor;
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * _speed;
+
+        motor.Move(_velocity);
+    }
+}
