3d6afad84546fd8e91e2d6b50901e6be1c48e77b
Adhiraj <adhirajsingh0105@gmail.com>
2020-05-13 12:54:36 +0530

    Created Local server

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5f;
+    [SerializeField]
+    private float _lookSensitivity = 3f;
 
     private PlayerMotor motor;
     void Start()
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Movement
         float _xMove = Input.GetAxisRaw("Horizontal");
         float _zMove = Input.GetAxisRaw("Vertical");
 
@@ -25,5 +28,19 @@ public class PlayerController : MonoBehaviour
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * _speed;
 
         motor.Move(_velocity);
+
+        //Rotation
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRotation, 0f) * _lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRotation, 0f, 0f) * _lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
     }
 }
---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsTDisable;
+
+    private Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsTDisable.Length; i++)
+            {
+                componentsTDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
