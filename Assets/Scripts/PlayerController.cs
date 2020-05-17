b8be05465c0aae4cd8375188a39adb7a2a79db91
Adhiraj <adhirajsingh0105@gmail.com>
2020-05-17 15:13:56 +0530

    Movement improvements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConfigurableJoint))]
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+    [SerializeField]
+    private float _lookSensitivity = 3f;
+
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [Header("Spring Settings:")]
+    [SerializeField]
+    private JointDriveMode jointMode = JointDriveMode.Position;
+    [SerializeField]
+    private float jointSpring = 20f;
+    [SerializeField]
+    private float jointMaxForce = 40f;
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        SetjointSettings(jointSpring);
+    }
+
+    void Update()
+    {
+        //Movement
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMove;
+        Vector3 _moveVertical = transform.forward * _zMove;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * _speed;
+
+        motor.Move(_velocity);
+
+        //Rotation
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRotation, 0f) * _lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRotation, 0f, 0f) * _lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+
+        Vector3 _thrusterForce = Vector3.zero;
+
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetjointSettings(0f);
+        }
+        else
+        {
+            SetjointSettings(jointSpring);
+        }
+
+        motor.Applythruster(_thrusterForce);
+    }
+
+    private void SetjointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive { mode = jointMode, positionSpring = jointSpring, maximumForce = jointMaxForce };
+    }
+}
